04450f4b7faf98b472544dbf528168f9178b6f5e
Naomi De Wys <146399253+naomidewys@users.noreply.github.com>
2024-08-09 13:51:54 +1000

    Uploaded main python file

---
+++ project-3-treasure-island.py
@@ -0,0 +1,48 @@
+print('''
+        ____________________________________________________________________
+ / \-----     ---------  -----------     -------------- ------    ----\
+ \_/__________________________________________________________________/
+ |~ ~~ ~~~ ~ ~ ~~~ ~ _____.----------._ ~~~  ~~~~ ~~   ~~  ~~~~~ ~~~~|
+ |  _   ~~ ~~ __,---'_       "         `. ~~~ _,--.  ~~~~ __,---.  ~~|
+ | | \___ ~~ /      ( )   "          "   `-.,' (') \~~ ~ (  / _\ \~~ |
+ |  \    \__/_   __(( _)_      (    "   "     (_\_) \___~ `-.___,'  ~|
+ |~~ \     (  )_(__)_|( ))  "   ))          "   |    "  \ ~~ ~~~ _ ~~|
+ |  ~ \__ (( _( (  ))  ) _)    ((     \\//    " |   "    \_____,' | ~|
+ |~~ ~   \  ( ))(_)(_)_)|  "    ))    //\\ " __,---._  "  "   "  /~~~|
+ |    ~~~ |(_ _)| | |   |   "  (   "      ,-'~~~ ~~~ `-.   ___  /~ ~ |
+ | ~~     |  |  |   |   _,--- ,--. _  "  (~~  ~~~~  ~~~ ) /___\ \~~ ~|
+ |  ~ ~~ /   |      _,----._,'`--'\.`-._  `._~~_~__~_,-'  |H__|  \ ~~|
+ |~~    / "     _,-' / `\ ,' / _'  \`.---.._          __        " \~ |
+ | ~~~ / /   .-' , / ' _,'_  -  _ '- _`._ `.`-._    _/- `--.   " " \~|
+ |  ~ / / _-- `---,~.-' __   --  _,---.  `-._   _,-'- / ` \ \_   " |~|
+ | ~ | | -- _    /~/  `-_- _  _,' '  \ \_`-._,-'  / --   \  - \_   / |
+ |~~ | \ -      /~~| "     ,-'_ /-  `_ ._`._`-...._____...._,--'  /~~|
+ | ~~\  \_ /   /~~/    ___  `---  ---  - - ' ,--.     ___        |~ ~|
+ |~   \      ,'~~|  " (o o)   "         " " |~~~ \_,-' ~ `.     ,'~~ |
+ | ~~ ~|__,-'~~~~~\    \"/      "  "   "    /~ ~~   O ~ ~~`-.__/~ ~~~|
+ |~~~ ~~~  ~~~~~~~~`.______________________/ ~~~    |   ~~~ ~~ ~ ~~~~|
+ |____~jrei~__~_______~~_~____~~_____~~___~_~~___~\_|_/ ~_____~___~__|
+ / \----- ----- ------------  ------- ----- -------  --------  -------\
+ \_/__________________________________________________________________/
+      ''')
+print("Welcome to Treasure Island")
+print("Your mission is to find the hidden treasure.")
+path = input("You arrive on the beach and walk towards a forked path. Do you go 'left' or 'right'? ")
+
+if path == "left":
+    print("You come across a lake.")
+    lake = input("Do you want to 'wait' for a boat or try to 'swim'? ")
+    if lake == "wait":
+        print("A lone traveller sails along on their boat and offers you a ride. You accept and make it to the other side.")
+        print("As you walk along, you notice three colourful trees.")
+        tree = input("Do you inspect the 'blue', 'red', or 'yellow' tree? ")
+        if tree == "yellow":
+            print("You circle the yellow tree and find a giant 'X' carved into the trunk. You look down and see a hidden door. You found the treasure!")
+        elif tree == "blue":
+            print("You walk towards the blue tree and look up. Angry monkeys throw rotten bananas at you. Game over!")
+        else:
+            print("You walk towards the red tree and don't notice the hidden fire pit. You fall. Game over!")
+    else:
+        print("You don't notice the creature lurking in the murky depths. It attacks you. Game over!")
+else:
+    print("You get trapped in quick sand. Game over!")
